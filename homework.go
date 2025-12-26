88577384e0e4f89440eea6252e620f88ac17c647
xiantingxinbu111 <1612236721@qq.com>
2025-12-26 19:09:15 +0800

    test

--- homework.go
+++ homework.go
@@ -3,8 +3,27 @@ package homework01
 // 1. 只出现一次的数字
 // 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
 func SingleNumber(nums []int) int {
-	// TODO: implement
-	return 0
+	var resultMap map[int]int = make[map[int]int]
+
+	for i:=0; i<len(nums); i++ {
+		if _, key := resultMap[nums[i]]; key {
+			resultMap[nums[i]] += 1
+		} else {
+			resultMap[nums[i]] = 1;
+		}
+	}
+
+	for key, value := range resultMap {
+		if value == 1 {
+			return key
+		}
+	}
+
+	return 0;
+}
+
+
+func singleNumberByMap(nums []int) int {
 }
 
 // 2. 回文数
